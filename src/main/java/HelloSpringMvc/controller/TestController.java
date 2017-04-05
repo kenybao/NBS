f4ea10cf7db4dd4800d12ad274345ccbdebd66c9
xukebao <xukebao@qjdchina.com>
2017-04-05 14:58:34 +0800

    firset add

---
+++ src/main/java/HelloSpringMvc/controller/TestController.java
@@ -0,0 +1,29 @@
+package HelloSpringMvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Administrator on 2016/8/29.
+ */
+
+@Controller
+@RequestMapping(value = "/Test/*") //访问的url地址前缀，可以不写，写了就必须在方法url前面先加上class url 进行区分控制器
+public class TestController
+{
+    //访问地址：http://localhost:8080/Test/returnSuccess
+    @RequestMapping(value = "returnSuccess")    //实际访问的url地址
+    public String returnSuccess() {
+        return "/Views/success";    //返回Views文件夹下的success.jsp页面
+    }
+
+    //访问地址：http://localhost:8080/Test/returnString
+    @RequestMapping(value = "returnString", produces = {"text/plain;charset=UTF-8"})
+    //produces用于解决返回中文乱码问题，application/json;为json解决中文乱码
+    @ResponseBody       //用于返回字符串,不写即返回视图
+    public String returnString() {
+
+        return "hello return string 这是中文，并没有乱码";
+    }
+}
---
+++ src/main/webapp/Views/success.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xukebao
+  Date: 2017-04-01
+  Time: 16:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>success.jsp</title>
+</head>
+<body>
+
+</body>
+</html>
