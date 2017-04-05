b0e2de33bb9ffcbf7d5016a8d42787aa194cc327
xukebao <xukebao@qjdchina.com>
2017-04-05 16:24:58 +0800

    test idea push

--- src/main/java/HelloSpringMvc/controller/TestController.java
+++ src/main/java/HelloSpringMvc/controller/TestController.java
@@ -23,7 +23,7 @@ public class TestController
     //produces用于解决返回中文乱码问题，application/json;为json解决中文乱码
     @ResponseBody       //用于返回字符串,不写即返回视图
     public String returnString() {
-//        ss
+//        ssss
         return "hello return string 这是中文，并没有乱码";
 
     }
