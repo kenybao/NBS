c58c431efc77acff7db5e2a1a161475d41dc45b6
kenybao <735980892@qq.com>
2017-09-26 19:42:21 +0800

    add some staff to ignore

--- src/main/java/HelloSpringMvc/controller/TestController.java
+++ src/main/java/HelloSpringMvc/controller/TestController.java
@@ -1,4 +1,4 @@
-package HelloSpringMvc.controller;
+package main.java.HelloSpringMvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
